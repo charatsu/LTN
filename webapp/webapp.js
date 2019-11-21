9d2f8495d42a1e96466bc305afcf9d362de3a648
Phạm Đỗ An Tâm <charatsu98@gmail.com>
2019-11-21 10:13:32 +0700

    fix bug

--- webapp/webapp.js
+++ webapp/webapp.js
@@ -5,7 +5,7 @@ angular.module('myApp', [
 ]).config(function($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: 'home.html',
-        controller: 'Home'
+        controller: 'home'
     });
 }).factory('mySocket', function (socketFactory) {
 	var myIoSocket = io.connect('/webapp');	//Tên namespace webapp
@@ -16,12 +16,12 @@ angular.module('myApp', [
 	return mySocket;
 	
 /////////////////////// Những dòng code ở trên phần này là phần cài đặt, các bạn hãy đọc thêm về angularjs để hiểu, cái này không nhảy cóc được nha!
-}).controller('Home', function($scope, mySocket) {
+}).controller('home', function($scope, mySocket) {
 	////Khu 1 -- Khu cài đặt tham số 
     //cài đặt một số tham số test chơi
 	//dùng để đặt các giá trị mặc định
     $scope.CamBienNhiet = "Không biết nữa ahihi, chưa thấy có thằng nào cập nhập hết";
-    $scope.leds_status = [1, 1]
+    $scope.leds_status = [1, 1, 1]
 	
 	////Khu 2 -- Cài đặt các sự kiện khi tương tác với người dùng
 	//các sự kiện ng-click, nhấn nút
@@ -54,7 +54,7 @@ angular.module('myApp', [
 	//// Khu 4 -- Những dòng code sẽ được thực thi khi kết nối với Arduino (thông qua socket server)
 	mySocket.on('connect', function() {
 		console.log("connected")
-		mySocket.emit("HOT") //Cập nhập trạng thái mưa
+		mySocket.emit("HOT") //Cập nhập trạng thái nhiệt
 	})
 		
 });
